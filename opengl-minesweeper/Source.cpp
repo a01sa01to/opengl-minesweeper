10753c5f57ac3eec57ccdc84f34f2ea656947dae
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-07-21 14:33:35 +0900

    foo

--- opengl-minesweeper/Source.cpp
+++ opengl-minesweeper/Source.cpp
@@ -1,8 +1,28 @@
+#include <string>
+#include <tuple>
+
 #include <glut.h>
 
 using namespace std;
+using ll = long long;
 
+// Settings
 constexpr int Interval = 20;
+constexpr pair<tuple<int, int, int>, tuple<int, int, int>> OverviewPos = { { 0, 0, 100 }, { 0, 0, 0 } };
+
+// State
+enum GameState
+{
+  GameStart,
+  GamePlaying,
+  GameOver,
+  GameClear
+};
+struct State {
+  GameState gameState = GameStart;
+  ll time = 0;
+};
+State state = State();
 
 void timer(int t) {
   glutPostRedisplay();
@@ -17,11 +37,29 @@ void reshape(int w, int h) {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(50, (double) w / h, 1, 1000);
+}
 
+void display_GameStart() {
   // Model view
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(100, 60, 100, 0, 0, 0, 0, 1, 0);
+  gluLookAt(0, 0, 100, 0, 0, 0, 0, 1, 0);
+
+  // Title
+  glPushMatrix();
+  {
+    glColor3d(0, 0, 0);
+    if (state.time < 2000) {
+      glRasterPos3d(0, 0, 0);
+    }
+    else {
+      double y = min(30.0, (state.time - 1000) / 1000.0 * 30);
+      glRasterPos3d(0, y, 0);
+    }
+    const string title = "MineSweeper";
+    for (char c : title) glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+  }
+  glPopMatrix();
 }
 
 void display() {
@@ -36,6 +74,17 @@ void display() {
   glColor3d(0, 1, 0), glBegin(GL_LINES), glVertex3d(0, 0, 0), glVertex3d(0, 100, 0), glEnd();
   // Z
   glColor3d(0, 0, 1), glBegin(GL_LINES), glVertex3d(0, 0, 0), glVertex3d(0, 0, 100), glEnd();
+
+  // Draw
+  if (state.gameState == GameStart) {
+    display_GameStart();
+  }
+
+  state.time += Interval;
+  if (state.time > 3000) {
+    state.time %= 1'000'000'000'000'000'000LL;  // 1e18
+    state.time += 3000;
+  }
 
   glutSwapBuffers();
 }
